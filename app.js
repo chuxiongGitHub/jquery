c07c0b32b23d12a481c6ac11ad23bc50015b795d
chencaihong <836908728@qq.com>
2016-03-28 11:37:42 +0800

    初始化项目

---
+++ app.js
@@ -0,0 +1,10 @@
+/**
+ * Created by rainbow on 2016/3/28 11:33.
+ * Knowledge is power.
+ */
+$(document).ready(function(){
+  // alert("文档已经加载完毕");
+    $("p").click(function(){
+       $(this).hide();
+    });
+});
