978ae61d2c576352a81fb40b0c4b6db5eba07d4c
chencaihong <836908728@qq.com>
2016-03-28 13:33:18 +0800

    jquery目标与冒泡

--- eventtarget.js
+++ eventtarget.js
@@ -2,3 +2,9 @@
  * Created by rainbow on 2016/3/28 13:17.
  * Knowledge is power.
  */
+$(document).ready(function(){
+$("body").bind("click",bodyHandler);
+});
+function bodyHandler(event){
+    console.log(event);
+}
